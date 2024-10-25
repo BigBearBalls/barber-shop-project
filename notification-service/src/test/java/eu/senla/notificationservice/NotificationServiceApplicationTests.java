1d5d7a1f3da63084ed74d83af6d13660f8f6720f
Viachaslau Tsarankou <tsarenkovslava@yandex.ru>
2024-10-25 16:48:26 +0300

    init notification-service

---
+++ notification-service/src/test/java/eu/senla/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.senla.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
