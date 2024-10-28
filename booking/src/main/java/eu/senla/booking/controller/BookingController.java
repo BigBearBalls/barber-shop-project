04b9260566c89dc7e876fff3a7c00dc40665fa61
Dany <daniil_zubets@senla.eu>
2024-10-28 17:16:52 +0300

    add booking endpoint (demo)

--- booking/src/main/java/eu/senla/booking/controller/BookingController.java
+++ booking/src/main/java/eu/senla/booking/controller/BookingController.java
@@ -8,9 +8,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/booking/")
 public class BookingController {
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello Barber";
+    // получает UUID мастера и возвращает список свободного времени для этого мастера
+    public void getMasterFreeTime() {
+
+    }
+
+    // получает конкретный день возвращает список свободного времени на этот день
+    public void getDateFreeTime() {
+
+    }
+
+    //Создаем бронь. получает id услуги (и мастера) и дату для записи. Возвращает id брони
+    public void bookTime() {
+
+    }
+
+    // удаляет бронь. Получает id, возвращает статус.
+    public void removeBook() {
+
+    }
+
+    // принимает id брони с новыми исправлениями. Возвращает статус (или измененную бронь)
+    public void changeBookDate() {
+
+    }
+
+    //принимает UUID пользователя и возвращает список его бронирований
+    public void showUsersBooks() {
+
+    }
+
+    //Получает id брони и возвращает всю информацию по ней
+    public void showBookInfoById() {
+
     }
 
 }
