1d39a559e3e1b7961c086a43c0aec0fd23593b99
Artsem Yushkevich <artsem_yushkevich@senla.eu>
2024-10-31 16:57:12 +0300

    .

---
+++ booking/src/main/java/eu/senla/booking/util/ExceptionMessageTemplateConstants.java
@@ -0,0 +1,12 @@
+package eu.senla.booking.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionMessageTemplateConstants {
+
+
+    public static final String MASTER_TIMETABLE_WITH_ID_NOT_FOUND_MSG_TEMPLATE = "Master timetable with id (%s) not found!";
+    public static final String BOOKING_WITH_ID_NOT_FOUND_MSG_TEMPLATE = "Booking with ID (%s) not found!";
+    public static final String DAY_TIME_WITH_ID_NOT_FOUND_MSG_TEMPLATE = "DayTime with ID (%s) not found!";
+}
