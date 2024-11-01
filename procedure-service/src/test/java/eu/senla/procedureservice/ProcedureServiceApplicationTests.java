5716ccbe1c333c51403645d7171a3bb8052db296
Artsem Yushkevich <artsem_yushkevich@senla.eu>
2024-11-01 14:57:26 +0300

    Добавлена централизованная настройка зависимостей

--- procedure-service/src/test/java/eu/senla/procedureservice/ProcedureServiceApplicationTests.java
+++ procedure-service/src/test/java/eu/senla/procedureservice/ProcedureServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package eu.senla.configserver;
+package eu.senla.procedureservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ProcedureServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
