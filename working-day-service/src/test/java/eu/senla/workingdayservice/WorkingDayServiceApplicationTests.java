3c2d910472d0ca887cdd54ce2090620589ad1ae1
Yevgany Tyurin <yevgany_tyurin@senla.eu>
2024-11-08 17:22:59 +0300

    init working-day-service

---
+++ working-day-service/src/test/java/eu/senla/workingdayservice/WorkingDayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.senla.workingdayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingDayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
