876110fbd7db5f8783a21d7966bf0e8135a29cac
Yevgany Tyurin <yevgany_tyurin@senla.eu>
2024-11-12 14:50:19 +0300

    add basic working day service

---
+++ working-day-service/src/main/java/eu/senla/workingday/WorkingDayServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.senla.workingday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingDayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingDayServiceApplication.class, args);
+	}
+
+}
---
+++ working-day-service/src/test/java/eu/senla/workingday/WorkingDayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.senla.workingday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class WorkingDayServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
