e9da5f02cadb5cf8b7097859c2800686bb149134
ChelovechekAD <80909920+ChelovechekAD@users.noreply.github.com>
2024-11-15 13:08:03 +0300

    fix exception with MasterHasProcedure entity (#65)
    Co-authored-by: Artsem Yushkevich <artsem_yushkevich@senla.eu>

--- procedure-service/src/main/java/eu/senla/procedureservice/data/entity/MasterHasProcedure.java
+++ procedure-service/src/main/java/eu/senla/procedureservice/data/entity/MasterHasProcedure.java
@@ -1,9 +1,6 @@
 package eu.senla.procedureservice.data.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.UUID;
@@ -16,11 +13,15 @@ import java.util.UUID;
 @Builder
 @Table(name = "masters_has_procedures", schema = "procedure_service_schema")
 public class MasterHasProcedure {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "master_execution_id")
     private Integer id;
+
     @Column(name = "master_id")
     private UUID masterId;
+
     @Column(name = "procedure_id")
     private Integer procedureId;
 }
