c6f84e76b93d13d865298de8d652e615bf89d5ae
Dany <daniil_zubets@senla.eu>
2024-11-18 13:01:34 +0300

    Finished calendar-service

--- calendar-service/src/main/java/eu/senla/calendarservice/controller/CalendarController.java
+++ calendar-service/src/main/java/eu/senla/calendarservice/controller/CalendarController.java
@@ -8,24 +8,24 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("api/v1/calendar/")
+@RequestMapping("api/v1/calendars/")
 @RequiredArgsConstructor
 public class CalendarController {
 
     private final CalendarService calendarService;
 
     @PostMapping("days-off/{day}")
-    public LocalDate setDayOff(@PathVariable @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate day) {
+    public LocalDate setDayOff(@PathVariable @DateTimeFormat(pattern = "yyyy-mm-dd") LocalDate day) {
         return calendarService.setDayOff(day);
     }
 
     @DeleteMapping("working-day/{day}")
-    public void cancelDayOff(@PathVariable @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate day) {
+    public void cancelDayOff(@PathVariable @DateTimeFormat(pattern = "yyyy-mm-dd") LocalDate day) {
         calendarService.setWorkingDay(day);
     }
 
     @GetMapping("{day}")
-    public Boolean checkDay(@PathVariable @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate day) {
+    public Boolean checkDay(@PathVariable @DateTimeFormat(pattern = "yyyy-mm-dd") LocalDate day) {
         return calendarService.checkDay(day);
     }
 }
--- calendar-service/src/main/java/eu/senla/calendarservice/exception/InvalidDateException.java
+++ calendar-service/src/main/java/eu/senla/calendarservice/exception/InvalidDateException.java
@@ -1,5 +1,6 @@
 package eu.senla.calendarservice.exception;
 
+
 public class InvalidDateException extends CalendarApiException {
     public InvalidDateException(String errorMessage, String errorCode) {
         super(errorMessage, errorCode);
