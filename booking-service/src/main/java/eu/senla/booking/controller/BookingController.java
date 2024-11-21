01bb0495b3075a6c69afc72c544a2bb12c8e1512
Dany <daniil_zubets@senla.eu>
2024-11-21 17:04:29 +0300

    Changed ALL v4

--- booking-service/src/main/java/eu/senla/booking/controller/BookingController.java
+++ booking-service/src/main/java/eu/senla/booking/controller/BookingController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/bookings")
+@RequestMapping("/api/v1/bookings/")
 @AllArgsConstructor
 @Validated
 public class BookingController {
