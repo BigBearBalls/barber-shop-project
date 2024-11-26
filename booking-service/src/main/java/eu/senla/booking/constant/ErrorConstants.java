7b84d9bf286f176d60003a09314897ffbe6a2bc7
ChelovechekAD <80909920+ChelovechekAD@users.noreply.github.com>
2024-11-27 01:21:03 +0300

    Features/bsp 87 (#88)

--- booking-service/src/main/java/eu/senla/booking/constant/ErrorConstants.java
+++ booking-service/src/main/java/eu/senla/booking/constant/ErrorConstants.java
@@ -2,7 +2,8 @@ package eu.senla.booking.constant;
 
 public class ErrorConstants {
 
-    public static final String HANDLE_RESOURCE_NOT_FOUND_BAD_REQUEST = "400001";
-    public static final String HANDLE_EXCEPTION = "400001";
-
+    public static final String HANDLE_UNKNOWN_EXCEPTION = "400001";
+    public static final String HANDLE_RESOURCE_NOT_FOUND_BAD_REQUEST = "400002";
+    public static final String HANDLE_FEIGN_CLIENT_EXCEPTION = "400003";
+    public static final String HANDLE_FEIGN_CLIENT_EXCEPTION_MESSAGE = "Feign client exception!";
 }
--- calendar-service/src/main/java/eu/senla/calendarservice/controller/handler/CalendarExceptionHandler.java
+++ calendar-service/src/main/java/eu/senla/calendarservice/controller/handler/CalendarExceptionHandler.java
@@ -1,10 +1,14 @@
 package eu.senla.calendarservice.controller.handler;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.senla.calendarservice.dto.ErrorResponse;
 import eu.senla.calendarservice.exception.CalendarApiException;
 import eu.senla.calendarservice.util.constants.ErrorConstants;
+import feign.FeignException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +16,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestControllerAdvice
+@Slf4j
+@RequiredArgsConstructor
 public class CalendarExceptionHandler {
 
+    private final ObjectMapper objectMapper;
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleBadDateFormat(HttpServletRequest request) {
@@ -38,6 +48,23 @@ public class CalendarExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseBuilder(
                 ErrorConstants.DATE_IS_ALREADY_EXIST_ERROR_MESSAGE, ErrorConstants.DATE_IS_ALREADY_EXIST_ERROR_CODE,
                 request.getRequestURI()));
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<?> handleFeignException(FeignException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.valueOf(e.status() != 0 ? e.status() : HttpStatus.INTERNAL_SERVER_ERROR.value());
+        ErrorResponse exceptionResponse = new ErrorResponse(LocalDateTime.now(), ErrorConstants.HANDLE_FEIGN_CLIENT_EXCEPTION,
+                ErrorConstants.HANDLE_FEIGN_CLIENT_EXCEPTION_MESSAGE, request.getRequestURI());
+        try {
+            Optional<ByteBuffer> responseBody = e.responseBody();
+            if (responseBody.isPresent()) {
+                String body = StandardCharsets.UTF_8.decode(responseBody.get()).toString();
+                exceptionResponse = objectMapper.readValue(body, ErrorResponse.class);
+            }
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+        }
+        return ResponseEntity.status(httpStatus).body(exceptionResponse);
     }
 
     @ExceptionHandler(Exception.class)
