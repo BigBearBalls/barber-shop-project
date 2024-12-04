e289fc043700393af16fe739b9a1d7c705257460
Artsem Yushkevich <artsem_yushkevich@senla.eu>
2024-12-04 14:37:20 +0300

    Fixed merge

--- account-service/src/main/java/eu/senla/accountservice/dto/exception/ExceptionResponse.java
+++
@@ -1,8 +0,0 @@
-package eu.senla.accountservice.dto.exception;
-
-import eu.senla.accountservice.enums.ErrorCode;
-
-import java.time.LocalDateTime;
-
-public record ExceptionResponse(LocalDateTime timestamp, ErrorCode code, String message, String path) {
-}
