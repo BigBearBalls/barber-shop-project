5d0d4c4d8c8031f0d549ea93e87cbc05ca3a25ea
Dany <daniil_zubets@senla.eu>
2024-12-06 11:22:59 +0300

    start of PLAHCTOH

--- booking-service/src/main/java/eu/senla/booking/controller/BookingController.java
+++ booking-service/src/main/java/eu/senla/booking/controller/BookingController.java
@@ -1,36 +1,36 @@
-package eu.senla.booking.controller;
-
-import eu.senla.booking.facade.BookingFacade;
-import eu.senla.common.booking.dto.request.BookingRequestDTO;
-import eu.senla.common.booking.dto.response.BookingResponseDTO;
-import eu.senla.common.booking.dto.response.IdResponseDTO;
-import eu.senla.common.constant.ValidationConstants;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
-
-@RestController
-@RequestMapping("/api/v1/bookings/")
-@AllArgsConstructor
-@Validated
-public class BookingController {
-
-    private final BookingFacade bookingFacade;
-
-    @PostMapping
-    public IdResponseDTO save(@RequestBody @Valid BookingRequestDTO bookingDTO) {
-        return bookingFacade.saveBooking(bookingDTO);
-    }
-
-    @GetMapping("{id}")
-    public BookingResponseDTO findById(@PathVariable
-                                       @NotNull(message = ValidationConstants.BOOKING_ID_CANNOT_BE_NULL_VALIDATION_MESSAGE)
-                                       UUID id) {
-        return bookingFacade.findById(id);
-    }
-
-}
+//package eu.senla.booking.controller;
+//
+//import eu.senla.booking.facade.BookingFacade;
+//import eu.senla.common.booking.dto.request.BookingRequestDTO;
+//import eu.senla.common.booking.dto.response.BookingResponseDTO;
+//import eu.senla.common.booking.dto.response.IdResponseDTO;
+//import eu.senla.common.constant.ValidationConstants;
+//import jakarta.validation.Valid;
+//import jakarta.validation.constraints.NotNull;
+//import lombok.AllArgsConstructor;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.UUID;
+//
+//@RestController
+//@RequestMapping("/api/v1/bookings/")
+//@AllArgsConstructor
+//@Validated
+//public class BookingController {
+//
+//    private final BookingFacade bookingFacade;
+//
+//    @PostMapping
+//    public IdResponseDTO save(@RequestBody @Valid BookingRequestDTO bookingDTO) {
+//        return bookingFacade.saveBooking(bookingDTO);
+//    }
+//
+//    @GetMapping("{id}")
+//    public BookingResponseDTO findById(@PathVariable
+//                                       @NotNull(message = ValidationConstants.BOOKING_ID_CANNOT_BE_NULL_VALIDATION_MESSAGE)
+//                                       UUID id) {
+//        return bookingFacade.findById(id);
+//    }
+//
+//}
--- booking-service/src/main/java/eu/senla/booking/facade/impl/BookingFacadeImpl.java
+++ booking-service/src/main/java/eu/senla/booking/facade/impl/BookingFacadeImpl.java
@@ -1,39 +1,39 @@
-package eu.senla.booking.facade.impl;
-
-import eu.senla.booking.entity.Booking;
-import eu.senla.booking.facade.BookingDataAggregator;
-import eu.senla.booking.facade.BookingFacade;
-import eu.senla.booking.service.BookingService;
-import eu.senla.common.booking.dto.request.AggregatedBooking;
-import eu.senla.common.booking.dto.request.BookingRequestDTO;
-import eu.senla.common.booking.dto.response.BookingResponseDTO;
-import eu.senla.common.booking.dto.response.IdResponseDTO;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.UUID;
-
-@Service
-@AllArgsConstructor
-public class BookingFacadeImpl implements BookingFacade {
-
-    private final BookingDataAggregator bookingDataAggregator;
-    private final BookingService bookingService;
-
-    @Override
-    public IdResponseDTO saveBooking(BookingRequestDTO bookingRequestDto) {
-
-        AggregatedBooking aggregatedBookingData = bookingDataAggregator
-                .collectDataForSaving(bookingRequestDto);
-
-        return bookingService.saveBooking(aggregatedBookingData);
-    }
-
-    @Override
-    public BookingResponseDTO findById(UUID id) {
-
-        Booking foundBooking = bookingService.findBookingById(id);
-
-        return bookingDataAggregator.collectResponseData(foundBooking);
-    }
-}
+//package eu.senla.booking.facade.impl;
+//
+//import eu.senla.booking.entity.Booking;
+//import eu.senla.booking.facade.BookingDataAggregator;
+//import eu.senla.booking.facade.BookingFacade;
+//import eu.senla.booking.service.BookingService;
+//import eu.senla.common.booking.dto.request.AggregatedBooking;
+//import eu.senla.common.booking.dto.request.BookingRequestDTO;
+//import eu.senla.common.booking.dto.response.BookingResponseDTO;
+//import eu.senla.common.booking.dto.response.IdResponseDTO;
+//import lombok.AllArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.UUID;
+//
+//@Service
+//@AllArgsConstructor
+//public class BookingFacadeImpl implements BookingFacade {
+//
+//    private final BookingDataAggregator bookingDataAggregator;
+//    private final BookingService bookingService;
+//
+//    @Override
+//    public IdResponseDTO saveBooking(BookingRequestDTO bookingRequestDto) {
+//
+//        AggregatedBooking aggregatedBookingData = bookingDataAggregator
+//                .collectDataForSaving(bookingRequestDto);
+//
+//        return bookingService.saveBooking(aggregatedBookingData);
+//    }
+//
+//    @Override
+//    public BookingResponseDTO findById(UUID id) {
+//
+//        Booking foundBooking = bookingService.findBookingById(id);
+//
+//        return bookingDataAggregator.collectResponseData(foundBooking);
+//    }
+//}
