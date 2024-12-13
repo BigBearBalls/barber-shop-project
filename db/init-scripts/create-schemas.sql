e1ae51c039ba0c67529dd33b336e68d97c383f35
Yevgany Tyurin <yevgany_tyurin@senla.eu>
2024-12-13 17:08:15 +0300

    remove meeting-room-service

--- db/init-scripts/create-schemas.sql
+++ db/init-scripts/create-schemas.sql
@@ -2,4 +2,3 @@ CREATE SCHEMA IF NOT EXISTS booking_service_schema;
 CREATE SCHEMA IF NOT EXISTS user_service_schema;
 CREATE SCHEMA IF NOT EXISTS calendar_service_schema;
 CREATE SCHEMA IF NOT EXISTS auth_service_schema;
-CREATE SCHEMA IF NOT EXISTS meeting_room_service_schema;
