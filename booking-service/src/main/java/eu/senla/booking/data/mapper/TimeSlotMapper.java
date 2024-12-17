fd6a7cc6a130f62c3fe079f68b470a5ad7db53a9
Dany <daniil_zubets@senla.eu>
2024-12-17 09:59:21 +0300

    working dev

--- booking-service/src/main/java/eu/senla/booking/data/mapper/TimeSlotMapper.java
+++ booking-service/src/main/java/eu/senla/booking/data/mapper/TimeSlotMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TimeSlotMapper {
 
-    TimeSlot toTimeSlot(TimeSlotResponseDTO timeSlotResponseDto);
+//    TimeSlot toTimeSlot(TimeSlotResponseDTO timeSlotResponseDto);
 
     TimeSlotResponseDTO toTimeSlotResponseDto(TimeSlot timeSlot);
 }
