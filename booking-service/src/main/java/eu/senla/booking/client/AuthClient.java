917f98141e15579a3a51888bc7492cc7d321a0ee
Dany <daniil_zubets@senla.eu>
2024-12-20 11:29:46 +0300

    Booking mail approve

---
+++ booking-service/src/main/java/eu/senla/booking/client/AuthClient.java
@@ -0,0 +1,15 @@
+package eu.senla.booking.client;
+
+import eu.senla.common.dto.UserCredentialsDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(url = "${feign.clients.auth-service.url}", name = "authClient")
+public interface AuthClient {
+
+    @GetMapping("internal/users/{userId}")
+    UserCredentialsDTO getUserById(@PathVariable UUID userId);
+}
